2bcf9b97e55a7f8bbb0fb85439ccf47f3678a60f
Roslynn Ricard <roslynnricard@gmail.com>
2023-04-16 23:08:17 -0700

    add getAllUsers API call to db

--- api.go
+++ api.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gofrs/uuid"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type user struct {
@@ -28,26 +31,78 @@ func NewServer() *Server {
 	return s
 }
 
-func (s *Server) CreateUser() http.HandlerFunc {
+func APIServer(url string, port string) *Server {
+
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/allUsers", getUsers) // To request all users
+	// r.HandleFunc("/user/{name}", user).Methods("GET")             // To request a specific user
+	// r.HandleFunc("createUser", CreateUser).Methods("POST") // To create a new user
+	// r.HandleFunc("deleteUser", DeleteUser).Methods("DELETE")      // To delete a user
+	// r.HandleFunc("changePassword", ChangePassword).Methods("PUT") // To change a user's password
+	// s := NewServer()
+	// s.HandleFunc("/api/users", s.CreateUser()).Methods(http.MethodPost)
+	log.Fatal(http.ListenAndServe(":10000", r))
+	return nil
+}
+
+func corsHandler(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var i user
-		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
 			return
 		}
-
-		i.ID = uuid.Must(uuid.NewV4())
-		s.users = append(s.users, i)
-
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(i); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		h(w, r)
 	}
 }
 
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	GetAllUsers(db)
+	fmt.Println("Endpoint hit: returnAllGroceries")
+}
+
+// func getUsers(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		switch r.Method {
+// 		case http.MethodGet:
+// 			// gorm for db read / write
+// 			db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			GetAllUsers(db)
+// 		default:
+// 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		}
+// 	}
+// }
+
+// func CreateUser(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		var i user
+// 		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		i.ID = uuid.Must(uuid.NewV4())
+// 		s.users = append(s.users, i)
+
+// 		w.Header().Set("Content-Type", "application/json")
+// 		if err := json.NewEncoder(w).Encode(i); err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+// 	}
+// }
+
 // TODO: func delete user
 // TODO: func get all users
+// TODO: func get a specific user
 // TODO: change password
 // TODO: get Goliath API key
---
+++ db.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UserRepo struct {
+	db *gorm.DB
+}
+
+// AutoMigrate will automatically migrate the database and correcrt schema errors on startup
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(&UserDB{})
+}
+
+// getAllUsers requests a list of users from the database
+func GetAllUsers(db *gorm.DB) ([]*UserDB, error) {
+	var users []*UserDB
+	result := db.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return users, nil
+}
+
+func CreateUser(db *gorm.DB) {
+	users := []UserDB{
+		{
+			ID:               "USER_ID.1",
+			Name:             "USER_NAME.1",
+			CreatedAt:        time.Now(),
+			Password:         "USER_PASSWORD.1",
+			SensorPrivateKey: "PRIVATE_KEY.1",
+		},
+		{
+			ID:               "USER_ID.2",
+			Name:             "USER_NAME.2",
+			CreatedAt:        time.Now(),
+			Password:         "USER_PASSWORD.2",
+			SensorPrivateKey: "PRIVATE_KEY.2",
+		},
+		{
+			ID:               "USER_ID.3",
+			Name:             "USER_NAME.3",
+			CreatedAt:        time.Now(),
+			Password:         "USER_PASSWORD.3",
+			SensorPrivateKey: "PRIVATE_KEY.3",
+		},
+	}
+	res := db.Create(&users)
+	if res.Error != nil {
+		panic(res.Error)
+	}
+	for _, user := range users {
+		fmt.Printf("school.ID: %d\n", user.ID)
+	}
+
+}
